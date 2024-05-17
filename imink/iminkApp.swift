33458e78a3f4c434096eb7b161c83376aece8536
jf666666 <jf6666666@icloud.com>
2024-05-17 08:52:12 +0800

    Initial Commit

---
+++ imink/iminkApp.swift
@@ -0,0 +1,17 @@
+//
+//  iminkApp.swift
+//  imink
+//
+//  Created by 姜锋 on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct iminkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
